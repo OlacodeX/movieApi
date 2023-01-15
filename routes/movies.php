120287d592ed1a5bca3b3442ac8fa97bc275ac2c
olacodex <aluko798@gmail.com>
2023-01-15 01:28:40 +0100

    pushing to git

--- routes/movies.php
+++ routes/movies.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 | Movie Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Here is where you can register movies routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
